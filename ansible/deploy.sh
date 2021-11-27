eb28f05d6c6a30edd33f5e2c23b14064e7af26cb
Tim Speetjens <tim.speetjens@gmail.com>
2021-11-27 14:08:28 +0100

    feat: update deploy command to run on nuc

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# not yet fully functional
-podman run --rm -v .:/runner:z quay.io/ansible/ansible-runner:stable-2.9-devel ansible-playbook -i inventory/ playbooks/configure-fitlet.yaml
+identity_file=${ANSIBLE_IDENTITY_FILE:-${HOME}/.ssh/id_rsa}
+mkdir -p ./.ssh
+cp ${identity_file} ./.ssh/
+ansible_identity_file=$(cd .ssh/ && pwd)/$(basename $identity_file)
+
+podman run --rm -v ${ansible_identity_file}:/runner/identity_file:z -v .:/runner:z \
+--env ANSIBLE_HOST_KEY_CHECKING=False \
+--env ANSIBLE_BECOME_PASS \
+quay.io/ansible/ansible-runner:stable-2.9-devel \
+ansible-playbook -i inventory/ --private-key=/runner/identity_file playbooks/configure-fitlet.yaml
